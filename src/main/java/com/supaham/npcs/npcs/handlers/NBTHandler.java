75fa019b5c67d01403407528af95f351ccc756b5
Ali Moghnieh <SupaHam.me@gmail.com>
2015-10-28 12:48:39 +0000

    Update spigot to 1.8.7-R0.1-SNAPSHOT.

--- src/main/java/com/supaham/npcs/npcs/handlers/NBTHandler.java
+++ src/main/java/com/supaham/npcs/npcs/handlers/NBTHandler.java
@@ -3,13 +3,13 @@ package com.supaham.npcs.npcs.handlers;
 import static com.supaham.npcs.npcs.NPCProperties.MINECRAFT_NBT;
 
 import com.supaham.npcs.NPCManager;
-
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-
 import com.supaham.npcs.events.NPCSpawnEvent;
 import com.supaham.npcs.npcs.NPCHandler;
 import com.supaham.npcs.utils.NMSUtils;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class NBTHandler extends NPCHandler {
 
@@ -20,14 +20,19 @@ public class NBTHandler extends NPCHandler {
   }
 
   @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
-  public void onNPCSpawn(NPCSpawnEvent event) {
-    String nbtString = getData(event.getData(), MINECRAFT_NBT, String.class);
-    if (nbtString != null) {
-      try {
-        NMSUtils.applyNBTString(event.getNpc(), nbtString);
-      } catch (Exception e) {
-        e.printStackTrace();
+  public void onNPCSpawn(final NPCSpawnEvent event) {
+    final String nbtString = getData(event.getData(), MINECRAFT_NBT, String.class);
+    new BukkitRunnable() {
+      @Override
+      public void run() {
+        if (nbtString != null) {
+          try {
+            NMSUtils.applyNBTString(event.getNpc(), nbtString);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
       }
-    }
+    }.runTaskLater(plugin, 2);
   }
 }
